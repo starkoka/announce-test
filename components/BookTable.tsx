1691e0b24f15ee018ba0dadf4028763e252f2ba8
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-06 20:51:38 +0900

    とりま適当にCSSを適用(当然PCのみ想定)

--- components/BookTable.tsx
+++ components/BookTable.tsx
@@ -1,17 +1,20 @@
 import { FC } from 'react';
 import { useBooks } from '@/hooks/useBooks';
+import styles from './BookTable.module.css'
 
 export const BookTable: FC = () => {
     const { isLoading, books} = useBooks();
     if (isLoading) return <p>Loading...</p>
 
     return(
-        <ul>
-            {books.map((book)=>(
-                <li key={book.id}>
-                    {book.title} / {book.author} / {book.price}
-                </li>
-            ))}
-        </ul>
+        <div className={styles.main}>
+            <ul>
+                {books.map((book)=>(
+                    <li key={book.id} className={styles.text}>
+                        {book.title} / {book.author} / {book.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import { getApp, FirebaseApp } from 'firebase/app'
 
 import '../utils/firebase/init' // Initialize FirebaseApp
@@ -8,7 +9,13 @@ import {BookTable} from "@/components/BookTable";
 const Home: NextPage = () => {
     const app: FirebaseApp = getApp()
     return (
-        <BookTable/>
+        <>
+            <Head>
+                <title>お知らせテスト</title>
+            </Head>
+            <h1>お知らせページ</h1>
+            <BookTable/>
+        </>
     )
 }
 
