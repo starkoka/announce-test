6fbb9c64c0f5575db773bff9c971a732e6ea7af8
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-06 18:18:42 +0900

    API keyとかを表示するようにしてみた

--- utils/firebase/init.ts
+++ utils/firebase/init.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// FirebaseApp インスタンスを初期化する
+initializeApp({
     apiKey: "AIzaSyDVOYX0sTt9dlqk_UUHKKToq8GdsEKzkS8",
     authDomain: "fir-test-539ee.firebaseapp.com",
     projectId: "fir-test-539ee",
@@ -14,8 +9,4 @@ const firebaseConfig = {
     messagingSenderId: "343075011997",
     appId: "1:343075011997:web:9bee3f006985755a164374",
     measurementId: "G-Q2YFJQZ1XV"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+})
